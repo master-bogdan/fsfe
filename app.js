386e2c22043ef3c23a2761bf5f3486123165d20c
Bogdan <maiskiychavk@gmail.com>
2023-06-27 16:59:03 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const http  = require('http');
+
+http.createServer((req, res) => {
+        res.write("On the way to being full stack");
+        res.end();
+
+}).listen(3000);
+
+console.log("Server started on port 3000");
